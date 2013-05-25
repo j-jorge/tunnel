4a6bcdfcf7b9a2c2c9a65fac3e77897b24b6931b
Julien Jorge <julien.jorge@stuff-o-matic.com>
2013-05-25 16:40:12 +0200

    Ajout des placeholders pour gentle slope.

--- tools/create-slope.sh
+++ tools/create-slope.sh
@@ -17,6 +17,25 @@ get_size_bump()
 EOF
 } # get_size_bump()
 
+generate_points_gentle()
+{
+    cat <<EOF
+0 0 0 0 176 0
+512 72 354 32 806 146
+1024 256 1024 256 1024 256
+2048 768 2048 768 2048 768
+2560 954 2265 878 2716 994
+3072 1024 2894 1024 3072 1024
+EOF
+} # generate_points_gentle()
+
+get_size_gentle()
+{
+    cat <<EOF
+3072 1280
+EOF
+} # get_size_gentle()
+
 if [ $# -le 1 ]
 then
     echo "usage $0 [ bump | gentle | sin | steep ] image_to_bend.png"
