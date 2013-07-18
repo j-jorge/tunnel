89799e7129d95d68aa4cae7cd4c64ad85e37e0c3
sangi <sebastien.angibaud@stuff-o-matic.com>
2013-07-18 16:12:51 +0200

    Ajout d'un level_exit

---
+++ lib/src/tunnel/code/level_exit.cpp
@@ -0,0 +1,84 @@
+/*
+  Copyright (C) 2012 Stuffomatic Ltd. <contact@stuff-o-matic.com>
+
+  All rights reserved.
+
+  See the accompanying license file for details about usage, modification and
+  distribution of this file.
+*/
+/**
+ * \file
+ * \brief Implementation of the tunnel::level_exit class.
+ * \author Sebastien Angibaud
+ */
+#include "tunnel/level_exit.hpp"
+
+#include "tunnel/game_variables.hpp"
+#include "tunnel/player.hpp"
+
+BASE_ITEM_EXPORT( level_exit, tunnel )
+
+/*----------------------------------------------------------------------------*/
+/**
+ * \brief Constructor.
+ */
+tunnel::level_exit::level_exit()
+: m_opened (false)
+{
+  set_can_move_items(false);
+} // level_exit::level_exit()
+
+/*----------------------------------------------------------------------------*/
+/**
+ * \brief Tell if the item is correctly initialized.
+ */
+bool tunnel::level_exit::is_valid() const
+{
+  return ! m_level_filename.empty();
+} // level_exit::is_valid()
+
+/*----------------------------------------------------------------------------*/
+/**
+ * \brief Set a field of type string.
+ * \param name The name of the field.
+ * \param value The new value of the field.
+ * \return false if the field "name" is unknow, true otherwise.
+ */
+bool tunnel::level_exit::set_string_field
+( const std::string& name, const std::string& value )
+{
+  bool result = true;
+
+  if ( name == "level_exit.next_level" )
+    m_level_filename = value;
+  else
+    result = super::set_string_field( name, value );
+
+  return result;
+} // level_exit::set_string_field()
+
+/*----------------------------------------------------------------------------*/
+/**
+ * \brief Call collision_check_and_align().
+ * \param that The other item of the collision.
+ * \param info Some informations about the collision.
+ */
+void tunnel::level_exit::collision
+( bear::engine::base_item& that, bear::universe::collision_info& info )
+{
+  if ( ! m_opened && 
+       game_variables::get_founded_crystal_count() >= 
+       game_variables::get_crystal_count() )
+    {
+      player* other = dynamic_cast<player*>(&that);
+      
+      if (other != NULL)
+        {
+          m_opened = true;
+          bear::engine::game::get_instance().set_waiting_level
+            ( m_level_filename );
+          
+          kill();
+        }
+    }
+} // level_exit::collision()
---
+++ lib/src/tunnel/level_exit.hpp
@@ -0,0 +1,62 @@
+/*
+  Copyright (C) 2012 Stuffomatic Ltd. <contact@stuff-o-matic.com>
+
+  All rights reserved.
+
+  See the accompanying license file for details about usage, modification and
+  distribution of this file.
+*/
+/**
+ * \file
+ * \brief A level_exit.
+ * \author Angibaud Sebastien
+ */
+#ifndef __TUNNEL_LEVEL_EXIT_HPP__
+#define __TUNNEL_LEVEL_EXIT_HPP__
+
+#include "tunnel/item_brick/transportable.hpp"
+#include "engine/base_item.hpp"
+#include "engine/item_brick/basic_renderable_item.hpp"
+#include "engine/item_brick/item_with_decoration.hpp"
+#include "engine/export.hpp"
+
+namespace tunnel
+{
+  /**
+   * \brief A level_exit.
+   *
+   *  - any field supported by the parent classes.
+   *
+   * \author Sebastien Angibaud
+   */
+  class level_exit:
+    public bear::engine::item_with_decoration
+  < bear::engine::basic_renderable_item< bear::engine::base_item> >,
+    public transportable
+  {
+    DECLARE_BASE_ITEM(level_exit);
+
+  public:
+    /** \brief The type of the parent class. */
+    typedef bear::engine::item_with_decoration
+    < bear::engine::basic_renderable_item< bear::engine::base_item> > super;
+
+  public:
+    level_exit();
+    
+    bool is_valid() const;
+    bool set_string_field( const std::string& name, const std::string& value );
+    
+    void collision
+    ( bear::engine::base_item& that, bear::universe::collision_info& info );
+
+  private:
+    /* \brief Indicates if the level_exit has been opened. */
+    bool m_opened;
+
+    /* \brief The filename of the next level. */
+    std::string m_level_filename;
+  }; // class level_exit
+} // namespace tunnel
+
+#endif // __TUNNEL_LEVEL_EXIT_HPP__
